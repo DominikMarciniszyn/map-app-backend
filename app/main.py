153b5533ca2cfd4260090df0e8ba4468770af02c
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2020-08-01 15:58:47 +0200

    feat: added main file which checks env variables

---
+++ app/main.py
@@ -0,0 +1,8 @@
+import os
+
+
+print(os.getenv('POSTGRES_DB'))
+print(os.getenv('POSTGRES_USER'))
+print(os.getenv('POSTGRES_PASSWORD'))
+print(os.getenv('DATABASE_HOST'))
+print(os.getenv('DATABASE_PORT'))
