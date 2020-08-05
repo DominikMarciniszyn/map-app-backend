827174178bca6b754870cfa864709da76925e89e
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2020-08-05 10:11:32 +0200

    refactor: removed unused code

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 from .routes import map_router
+from .model.map import Map
+from .model.map_point import MapPoint
+
+
+Map.create_table()
+MapPoint.create_table()
 
 
 app = FastAPI()
