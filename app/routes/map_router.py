9e442405f915e4674a3d52262cc3c5386686c923
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2020-08-05 15:35:31 +0200

    feat: added more crud methods

--- app/routes/map_router.py
+++ app/routes/map_router.py
@@ -1,19 +1,28 @@
 from fastapi import APIRouter
-from ..model.map import Map
 from ..schemas.map import Map as MapSchema
+from ..services.map_service import MapService
 
 
 router = APIRouter()
+map_service = MapService()
 
 
 @router.post('/map/', tags=['map'])
-async def create_map(map: MapSchema):
-    db_map = Map(
-        0,
-        name=map.name,
-        latitude=map.latitude,
-        longtitude=map.longitude,
-        zoom_level=map.zoom
-    )
-    db_map.save()
-    return db_map
+def create_map(map: MapSchema):
+    map_object = map_service.create_map(map)
+    return map_object
+
+
+@router.get('/maps/', tags=['map'])
+def get_all_maps():
+    maps = map_service.get_maps()
+    return maps
+
+
+@router.get('/map/{id}')
+def get_map(id: int):
+    map_object = map_service.get_map_by_id(id)
+    return map_object
+
+
+
